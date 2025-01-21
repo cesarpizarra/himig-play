03fbed8f6e5107bf926d9420175a936d384f64b5
Cesar Pizarra <cpizarra002@gmail.com>
2025-01-21 18:27:47 +0800

    Create route.ts

---
+++ app/api/spotify/playing/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+import { getAuthenticatedSession } from "@/app/util/auth";
+import spotifyApi, { setSpotifyAccessToken } from "@/app/util/spotifyApi";
+
+export async function GET() {
+  try {
+    const accessToken = await getAuthenticatedSession();
+
+    setSpotifyAccessToken(accessToken);
+
+    const playing = await spotifyApi.getMyCurrentPlayingTrack();
+
+    return NextResponse.json(playing.body);
+  } catch (error: any) {
+    console.error("Error fetching Spotify user data:", error.message);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
