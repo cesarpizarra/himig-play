ab0aa8f687c5c276290cb057ee11c6882d6e2138
Cesar Pizarra <cpizarra002@gmail.com>
2025-01-21 18:28:35 +0800

    Update page.tsx

--- app/himig/home/page.tsx
+++ app/himig/home/page.tsx
@@ -1,5 +1,18 @@
 "use client";
+import Card from "@/app/components/card/Card";
+import Card2 from "@/app/components/card/Card2";
+import SkeletonLoader from "@/app/components/common/SkeletonLoader";
+import {
+  useAlbums,
+  useNewReleases,
+  usePlaylist,
+  useRecent,
+  useTopArtists,
+  useTracks,
+} from "@/app/hooks/useSpotify";
+import Link from "next/link";
 import React, { useState } from "react";
+import { FaHeart } from "react-icons/fa";
 
 const dummyData = [
   { id: 1, name: "Liked Songs" },
@@ -25,9 +38,29 @@ const colors: string[] = [
 
 export default function Home() {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const {
+    data: topArtists,
+    isLoading: topLoading,
+    error: topError,
+  } = useTopArtists();
+  const {
+    data: newReleases,
+    isLoading: newReleasesLoading,
+    error: newReleasesError,
+  } = useNewReleases();
+  const { data: albumData, isLoading: albumLoading } = useAlbums();
+  const { data: playlistData, isLoading: playlistLoading } = usePlaylist();
+
+  const {
+    data: recents,
+    isLoading: recentLoading,
+    error: recentError,
+  } = useRecent();
+  const playlists = playlistData?.items || [];
+  const albums = albumData?.items || [];
 
   return (
-    <section className="min-h-screen w-full rounded-md bg-base-200">
+    <div className="min-h-screen w-full rounded-md bg-base-200">
       <div
         style={{
           background: hoveredIndex !== null ? colors[hoveredIndex] : "#102693",
@@ -37,18 +70,89 @@ export default function Home() {
         <div className="pointer-events-none absolute inset-0 z-[-1] bg-gradient-to-b from-transparent to-base-200"></div>
         <h1>Good Evening!</h1>
 
-        <div className="mt-4 grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4">
-          {dummyData.map((value, index) => (
-            <div
-              key={value.id}
-              className="cursor-pointer rounded-md bg-gray-100 bg-opacity-10 bg-clip-padding p-4 backdrop-blur-sm backdrop-filter transition-all duration-300 hover:bg-opacity-15"
-              onMouseOver={() => setHoveredIndex(index)}
-            >
-              {value.name}
-            </div>
-          ))}
+        <div className="mt-4 grid grid-cols-2 gap-3 lg:grid-cols-4">
+          {playlistLoading || albumLoading ? (
+            <SkeletonLoader count={1} />
+          ) : (
+            <Link href="/himig/liked-songs">
+              <div
+                className="flex cursor-pointer items-center gap-3 rounded-md bg-gray-100 bg-opacity-10 bg-clip-padding backdrop-blur-sm backdrop-filter transition-all duration-300 hover:bg-opacity-15"
+                onMouseOver={() => setHoveredIndex(colors.length - 1)}
+              >
+                <div className="rounded-md bg-gradient-to-r from-purple-800 to-white p-4">
+                  <FaHeart size={16} className="text-white" />
+                </div>
+                <div>
+                  <p className="text-xs">Liked Songs</p>
+                </div>
+              </div>
+            </Link>
+          )}
+          {playlistLoading || albumLoading ? (
+            <SkeletonLoader count={7} />
+          ) : playlists.length > 0 ? (
+            playlists.map((playlist, index) => (
+              <Link key={playlist.id} href={`/himig/playlist/${playlist.id}`}>
+                <div
+                  className="flex cursor-pointer items-center gap-3 rounded-md bg-gray-100 bg-opacity-10 bg-clip-padding backdrop-blur-sm backdrop-filter transition-all duration-300 hover:bg-opacity-15"
+                  onMouseOver={() => setHoveredIndex(index)}
+                >
+                  <img
+                    src={playlist.images[0]?.url}
+                    alt={playlist.name}
+                    className="h-12 w-12 rounded-l-md object-cover"
+                  />
+                  <div>
+                    <p className="text-xs"> {playlist.name}</p>
+                  </div>
+                </div>
+              </Link>
+            ))
+          ) : (
+            <div>No playlists found.</div>
+          )}
+
+          {albums.length > 0 &&
+            albums.map((album, index) => (
+              <Link key={index} href={`/himig/album/${album.album.id}`}>
+                <div
+                  className="flex cursor-pointer items-center gap-3 rounded-md bg-gray-100 bg-opacity-10 bg-clip-padding backdrop-blur-sm backdrop-filter transition-all duration-300 hover:bg-opacity-15"
+                  onMouseOver={() => setHoveredIndex(index)}
+                >
+                  <img
+                    src={album.album.images[0].url}
+                    alt={album.album.name}
+                    className="h-12 w-12 rounded-l-md object-cover"
+                  />
+                  <div>
+                    <p className="text-xs"> {album.album.name}</p>
+                  </div>
+                </div>
+              </Link>
+            ))}
         </div>
       </div>
-    </section>
+
+      <Card2
+        data={newReleases}
+        title="New Releases"
+        isLoading={newReleasesLoading}
+        error={newReleasesError}
+      />
+
+      <Card
+        data={recents?.items}
+        title="Recently Played"
+        isLoading={recentLoading}
+        error={recentError}
+      />
+
+      <Card2
+        data={topArtists}
+        title="Top Artists"
+        isLoading={topLoading}
+        error={topError}
+      />
+    </div>
   );
 }
