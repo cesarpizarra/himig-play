fc86fc4615b35235dbd297cb6882c375082254b6
Cesar Pizarra <cpizarra002@gmail.com>
2025-01-21 18:28:53 +0800

    Update spotify.ts

--- app/types/spotify.ts
+++ app/types/spotify.ts
@@ -1,14 +1,72 @@
-export type PlaylistItem = {
+export type DefaultItem = {
   id: string;
   name: string;
   description: string;
   images: Array<{ url: string }>;
   external_urls: { spotify: string };
+  type: string;
+};
+
+export type DefaultItem2 = {
+  images: Array<{ url: string }>;
+  name: string;
+  external_urls: { spotify: string };
+  artists: Array<{ name: string }>;
+  release_date: string;
+  release_date_precision: string;
+  total_tracks: number;
+  type: string;
+  id: string;
 };
 
 export type PlaylistResponse = {
-  items: PlaylistItem[];
+  items: DefaultItem[];
   total: number;
+};
+
+export type AlbumResponse = {
+  items: Array<{
+    album: DefaultItem2;
+  }>;
+  total: number;
+  offset: number;
+};
+
+export type AlbumById = {
+  images: Array<{ url: string }>;
+  name: string;
+  external_urls: { spotify: string };
+  artists: Array<{ name: string }>;
+  tracks: {
+    items: Array<{
+      id: string;
+      name: string;
+      duration_ms: number;
+    }>;
+  };
+  type: string;
+  id: string;
+  label: string;
+  total_tracks: number;
+};
+
+export type PlaylistById = {
+  images: Array<{ url: string }>;
+  name: string;
+  external_urls: { spotify: string };
+  artists: Array<{ name: string }>;
+  tracks: {
+    items: Array<{
+      track: {
+        album: DefaultItem2;
+        id: string;
+        name: string;
+        duration_ms: number;
+      };
+    }>;
+  };
+  type: string;
+  id: string;
 };
 
 export type TopArtist = {
@@ -27,4 +85,45 @@ export type TopTrack = {
   };
   artists: Array<{ name: string }>;
   external_urls: { spotify: string };
+};
+
+export type CurrentPlayingTrack = {
+  current_playing_type: string;
+  is_playing: boolean;
+  item: {
+    name: string;
+    album: {
+      images: Array<{ url: string }>;
+      external_urls: { spotify: string };
+      release_date: string;
+      release_date_precision: string;
+    };
+    artists: Array<{
+      name: string;
+      external_urls: { spotify: string };
+    }>;
+    external_urls: { spotify: string };
+  };
+};
+
+export type LikeSongsResponse = {
+  items: Array<{
+    track: {
+      album: DefaultItem2;
+      name: string;
+      duration_ms: number;
+    };
+  }>;
+  total: number;
+  offset: number;
+};
+
+export type RecentResponse = {
+  items: Array<{
+    track: {
+      album: DefaultItem2;
+    };
+  }>;
+  total: number;
+  offset: number;
 };
