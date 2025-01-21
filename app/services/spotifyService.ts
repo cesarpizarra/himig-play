4a9d509f7001827f211b6157f809d8f02356a548
Cesar Pizarra <cpizarra002@gmail.com>
2025-01-21 18:28:50 +0800

    Update spotifyService.ts

--- app/services/spotifyService.ts
+++ app/services/spotifyService.ts
@@ -1,11 +1,8 @@
 import axiosInstance from "../axios/axiosIntstance";
-import { PlaylistResponse, TopArtist, TopTrack } from "../types/spotify";
 
 export const getPlaylist = async () => {
   try {
-    const response = await axiosInstance.get<PlaylistResponse>(
-      "/api/spotify/playlist",
-    );
+    const response = await axiosInstance.get("/api/spotify/playlist");
     return response.data;
   } catch (error) {
     console.error("Error getting playlist", error);
@@ -13,9 +10,9 @@ export const getPlaylist = async () => {
   }
 };
 
-export const getTopArtists = async () => {
+export const getTop3Artists = async () => {
   try {
-    const response = await axiosInstance.get<TopArtist[]>("/api/spotify/top");
+    const response = await axiosInstance.get("/api/spotify/top");
     return response.data;
   } catch (error) {
     console.error("Error getting top artists", error);
@@ -25,9 +22,7 @@ export const getTopArtists = async () => {
 
 export const getTopTracks = async () => {
   try {
-    const response = await axiosInstance.get<TopTrack[]>(
-      "/api/spotify/top/tracks",
-    );
+    const response = await axiosInstance.get("/api/spotify/top/tracks");
     return response.data;
   } catch (error) {
     console.error("Error getting top tracks", error);
@@ -41,6 +36,86 @@ export const getFollowed = async () => {
     return response.data;
   } catch (error) {
     console.error("Error getting top tracks", error);
+    throw error;
+  }
+};
+
+export const getCurrentPlaying = async () => {
+  try {
+    const response = await axiosInstance.get("/api/spotify/playing");
+    return response.data;
+  } catch (error) {
+    console.error("Error getting current playing", error);
+    throw error;
+  }
+};
+
+export const getAlbums = async () => {
+  try {
+    const response = await axiosInstance.get("/api/spotify/albums");
+    return response.data;
+  } catch (error) {
+    console.error("Error getting albums", error);
+    throw error;
+  }
+};
+
+export const getTracks = async () => {
+  try {
+    const response = await axiosInstance.get("/api/spotify/tracks");
+    return response.data;
+  } catch (error) {
+    console.error("Error getting tracks", error);
+    throw error;
+  }
+};
+
+export const getRecentlyPlayed = async () => {
+  try {
+    const response = await axiosInstance.get("/api/spotify/recently-played");
+    return response.data;
+  } catch (error) {
+    console.error("Error getting recently played", error);
+    throw error;
+  }
+};
+
+export const getTopArtists = async () => {
+  try {
+    const response = await axiosInstance.get("/api/spotify/top/artists");
+    return response.data;
+  } catch (error) {
+    console.error("Error getting recently played", error);
+    throw error;
+  }
+};
+
+export const getNewReleases = async () => {
+  try {
+    const response = await axiosInstance.get("/api/spotify/new-releases");
+    return response.data;
+  } catch (error) {
+    console.error("Error getting recently played", error);
+    throw error;
+  }
+};
+
+export const getAlbumById = async (id: string) => {
+  try {
+    const response = await axiosInstance.get(`/api/spotify/albums/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error getting album", error);
+    throw error;
+  }
+};
+
+export const getPlaylistById = async (id: string) => {
+  try {
+    const response = await axiosInstance.get(`/api/spotify/playlist/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error getting album", error);
     throw error;
   }
 };
