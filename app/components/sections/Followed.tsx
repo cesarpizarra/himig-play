62789d627a859c26254a6d688818c344b910cdb0
Cesar Pizarra <cpizarra002@gmail.com>
2025-01-22 23:43:09 +0800

    Update Followed.tsx

--- app/components/sections/Followed.tsx
+++ app/components/sections/Followed.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { TopArtist } from "@/app/types/spotify";
 import SkeletonLoader from "@/app/components/common/SkeletonLoader";
+import usePlaySong from "@/app/hooks/usePlaySong";
+import { FaPlay } from "react-icons/fa";
+import Link from "next/link";
 
 interface Props {
   following: TopArtist[] | null | undefined;
@@ -13,6 +16,8 @@ const FollowedArtistsSection: React.FC<Props> = ({
   isLoading,
   error,
 }) => {
+  const { handlePlay } = usePlaySong();
+
   if (error) return <div>Error loading followed artists: {error.message}</div>;
 
   return (
@@ -27,19 +32,29 @@ const FollowedArtistsSection: React.FC<Props> = ({
           following?.map((artist) => (
             <div
               key={artist.id}
-              className="flex transform cursor-pointer flex-col items-center p-2 transition-transform duration-200 ease-in-out hover:scale-105 hover:bg-base-100"
+              className="group flex transform cursor-pointer flex-col items-center p-2 transition-transform duration-200 ease-in-out hover:scale-105 hover:bg-base-100"
             >
-              <img
-                src={artist.images[0]?.url}
-                alt={artist.name}
-                className="h-32 w-32 rounded-full object-cover shadow-lg md:h-40 md:w-40"
-              />
-              <div className="mt-2 text-center text-sm font-semibold text-white">
-                <h3>{artist.name}</h3>
-                <p className="text-xs capitalize text-gray-400">
-                  {artist.type}
-                </p>
-              </div>
+              <Link href={`/himig/artist/#`}>
+                <img
+                  src={artist.images[0]?.url}
+                  alt={artist.name}
+                  className="h-32 w-32 rounded-full object-cover shadow-lg md:h-40 md:w-40"
+                />
+                <div className="mt-2 text-center text-sm font-semibold text-white">
+                  <h3>{artist.name}</h3>
+                  <p className="text-xs capitalize text-gray-400">
+                    {artist.type}
+                  </p>
+                </div>
+              </Link>
+
+              {/* Play button */}
+              <button
+                onClick={() => handlePlay(artist.uri)}
+                className="absolute bottom-2 right-2 flex h-10 w-10 items-center justify-center rounded-full bg-green-500 p-3 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+              >
+                <FaPlay className="h-6 w-6 text-black" />
+              </button>
             </div>
           ))
         )}
