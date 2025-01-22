2769262ada0f8effb9f3998329e545f3f12a373b
Cesar Pizarra <cpizarra002@gmail.com>
2025-01-22 23:42:36 +0800

    Create route.ts

---
+++ app/api/spotify/artist/album/[id]/route.ts
@@ -0,0 +1,29 @@
+import { getAuthenticatedSession } from "@/app/util/auth";
+import spotifyApi, { setSpotifyAccessToken } from "@/app/util/spotifyApi";
+import { NextResponse } from "next/server";
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: number } },
+) {
+  try {
+    const { id } = params;
+
+    const accessToken = await getAuthenticatedSession();
+
+    setSpotifyAccessToken(accessToken);
+
+    const album = await spotifyApi.getArtistAlbums(String(id));
+
+    return NextResponse.json(album.body);
+  } catch (error) {
+    console.error("Failed to retrieve spotify data:", error);
+    return NextResponse.json(
+      {
+        status: false,
+        message: "Failed to retrieve spotify data",
+      },
+      { status: 500 },
+    );
+  }
+}
