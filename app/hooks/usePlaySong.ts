5513f90130486e197864a47e0857c8be32e09c14
Cesar Pizarra <cpizarra002@gmail.com>
2025-01-22 23:43:53 +0800

    Create usePlaySong.ts

---
+++ app/hooks/usePlaySong.ts
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { playSong } from "@/app/services/spotifyService";
+const usePlaySong = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const handlePlay = async (songUri: string) => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      await playSong(songUri);
+    } catch (err: any) {
+      console.error("Failed to play song:", err);
+      setError("Failed to play song");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { handlePlay, loading, error };
+};
+
+export default usePlaySong;
