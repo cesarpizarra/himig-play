b53e55d04436bed0078900cb8f4fbd50a0e95360
Cesar Pizarra <cpizarra002@gmail.com>
2025-01-22 23:43:19 +0800

    Update TopArtists.tsx

--- app/components/sections/TopArtists.tsx
+++ app/components/sections/TopArtists.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { TopArtist } from "@/app/types/spotify";
 import SkeletonLoader from "@/app/components/common/SkeletonLoader";
 import Link from "next/link";
+import usePlaySong from "@/app/hooks/usePlaySong";
+import { FaPlay } from "react-icons/fa";
 
 interface Props {
   topArtists: TopArtist[] | null | undefined;
@@ -14,18 +16,15 @@ const TopArtistsSection: React.FC<Props> = ({
   isLoading,
   error,
 }) => {
+  const { handlePlay } = usePlaySong();
+
   if (error) return <div>Error loading top artists: {error.message}</div>;
 
   return (
     <div className="p-6">
-      <div className="mb-4 flex items-center justify-between">
-        <h1 className="text-md font-semibold text-white md:text-lg">
-          Top Artists This Month
-        </h1>
-        <Link href="#">
-          <p className="text-sm text-gray-400 hover:underline">Show all</p>
-        </Link>
-      </div>
+      <h1 className="text-md mb-4 font-semibold text-white md:text-lg">
+        Top Artists This Month
+      </h1>
       <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
         {isLoading ? (
           <SkeletonLoader count={4} itemClassName="custom-item-class" />
@@ -33,19 +32,29 @@ const TopArtistsSection: React.FC<Props> = ({
           topArtists?.map((artist) => (
             <div
               key={artist.id}
-              className="flex transform cursor-pointer flex-col items-center p-2 transition-transform duration-200 ease-in-out hover:scale-105 hover:bg-base-100"
+              className="group flex transform cursor-pointer flex-col items-center p-2 transition-transform duration-200 ease-in-out hover:scale-105 hover:bg-base-100"
             >
-              <img
-                src={artist.images[0]?.url}
-                alt={artist.name}
-                className="h-32 w-32 rounded-full object-cover shadow-lg md:h-40 md:w-40"
-              />
-              <div className="mt-2 text-center text-sm font-semibold text-white">
-                <h3>{artist.name}</h3>
-                <p className="text-xs capitalize text-gray-400">
-                  {artist.type}
-                </p>
-              </div>
+              <Link href={`/himig/artist/#`}>
+                <img
+                  src={artist.images[0]?.url}
+                  alt={artist.name}
+                  className="h-32 w-32 rounded-full object-cover shadow-lg md:h-40 md:w-40"
+                />
+                <div className="mt-2 text-center text-sm font-semibold text-white">
+                  <h3>{artist.name}</h3>
+                  <p className="text-xs capitalize text-gray-400">
+                    {artist.type}
+                  </p>
+                </div>
+              </Link>
+
+              {/* Play button */}
+              <button
+                onClick={() => handlePlay(artist.uri)}
+                className="absolute bottom-2 right-2 flex h-10 w-10 items-center justify-center rounded-full bg-green-500 p-3 text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+              >
+                <FaPlay className="h-6 w-6 text-black" />
+              </button>
             </div>
           ))
         )}
