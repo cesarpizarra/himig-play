cca9a888c29bab769f2c87162c84bee73f0770f7
Cesar Pizarra <cpizarra002@gmail.com>
2025-01-23 22:44:02 +0800

    Create SearchContext.tsx

---
+++ app/context/SearchContext.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React, { createContext, useContext, useState } from "react";
+
+interface SearchContextType {
+  searchQuery: string;
+  setSearchQuery: (query: string) => void;
+}
+
+const SearchContext = createContext<SearchContextType | undefined>(undefined);
+
+export const useSearchContext = () => {
+  const context = useContext(SearchContext);
+  if (!context) {
+    throw new Error("useSearchContext must be used within a SearchProvider");
+  }
+  return context;
+};
+
+export const SearchProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  return (
+    <SearchContext.Provider value={{ searchQuery, setSearchQuery }}>
+      {children}
+    </SearchContext.Provider>
+  );
+};
