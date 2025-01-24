7b174af4976b7259fdd5e8d76401f943487a07aa
Cesar Pizarra <cpizarra002@gmail.com>
2025-01-24 19:16:09 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      "i.scdn.co",
+      "mosaic.scdn.co",
+      "t.scdn.co",
+      "thisis-images.scdn.co",
+      "seed-mix-image.spotifycdn.com",
+      "spotifycdn.com",
+      "lineup-images.scdn.co",
+      "image-cdn-ak.spotifycdn.com",
+    ],
+  },
+};
 
 export default nextConfig;
